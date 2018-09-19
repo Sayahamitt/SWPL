e0f910e12a3f0fe65f549b7c20b3dd9faf10bd94
h.yamashita <hiro0gjm0roki@gmail.com>
2018-09-19 16:14:22 +0900

    Wavefieldクラスを整理した
    インナークラス FiledParamater を削除した. このクラスは光波場のパラメータを保持する目的で作成したが、FiledAxisクラスと重複していたので廃止.
    メンバ変数 sizex,sizey,pitchx,pitchy,をメンバ変数であるFiledAxisクラスのインスタンスの値を取得するメンバ関数にした.

--- src/SWPL.hpp
+++ src/SWPL.hpp
@@ -14,70 +14,11 @@
 
 class Wavefield{
 	std::vector<std::complex<double>> field;//1次元配列に2次元データを行優先で充填する
-	//std::vector<double> xaxis;
-	//std::vector<double> yaxis;
-	int sizex;
-	int sizey;
-	double pitchx;
-	double pitchy;
 	double wvl;
 	double wvnum;
 	std::complex<double> jK;
+public:
 	static const std::complex<double> imunt;
-public:/*
-	class FieldParamater{
-		unsigned int sizex;
-		unsigned int sizey;
-		double pitchx;
-		double pitchy;
-		double wvl;
-		double wvnum;
-		std::complex<double> jK;
-		//std::vector<double> xaxis;
-		//std::vector<double> yaxis;
-	public:
-		FieldParamater(unsigned int arg_sizex, unsigned int arg_sizey, double arg_pitchx, double arg_pitchy, double arg_wvl):
-		sizex(arg_sizex),
-		sizey(arg_sizey),
-		pitchx(arg_pitchx),
-		pitchy(arg_pitchy),
-		wvl(arg_wvl),
-		wvnum(2*SWPL_PI/arg_wvl),
-		jK(imunt*(2*SWPL_PI/arg_wvl)){
-			//X,Y軸の生成
-			for(double ii=-(sizex/2);xaxis.size()<sizex; ii++){
-				xaxis.push_back(ii*pitchx);
-			}
-			for(long long ii=-(sizey/2);yaxis.size()<sizey; ii++){
-				yaxis.push_back(ii*pitchy);
-			}
-		}
-
-		unsigned int SizeX() const{
-			return sizex;
-		}
-		unsigned int SizeY() const{
-			return sizey;
-		}
-		double PtithX() const{
-			return pitchx;
-		}
-		double PtithY() const{
-			return pitchy;
-		}
-		double Wavelength() const{
-			return wvl;
-		}
-		double WaveNumber() const{
-			return wvnum;
-		}
-		const std::vector<double> AxisX(){
-			return xaxis;
-		}
-		const std::vector<double> AxisY(){
-			return yaxis;
-		}
-	};*/
 
 	class FieldAxis:public std::vector<double>{
 		double pitch;
@@ -97,7 +38,6 @@ public:/*
 			for(double value=lower;value<=upper; value+=arg_pitch){
 				push_back(value);
 				if(value==upper){
-					std::cout<<"end!!"<<std::endl;
 				}
 			}
 		}
@@ -155,56 +95,80 @@ private:
 public:
 	Wavefield(int xsize, int ysize, double gridspace, double wavelength):
 	field(std::vector<std::complex<double>>(ysize*xsize, std::complex<double>(0))),
-	sizex(xsize),
-	sizey(ysize),
-	pitchy(gridspace),
-	pitchx(gridspace),
 	wvl(wavelength),
 	wvnum(2*SWPL_PI/wavelength),
 	jK(imunt*(2*SWPL_PI/wavelength)),
 	xaxis(xsize,gridspace),
 	yaxis(ysize,gridspace){
 	}
-
+	Wavefield(std::vector<std::complex<double>> arg_field, Wavefield::FieldAxis arg_xaxis, Wavefield::FieldAxis arg_yaxis, double wavelength):
+	field(arg_field),
+	wvl(wavelength),
+	wvnum(2*SWPL_PI/wavelength),
+	jK(imunt*(2*SWPL_PI/wavelength)),
+	xaxis(arg_xaxis),
+	yaxis(arg_yaxis){
+	}
 	//Copy Constructor
 	Wavefield(const Wavefield& rhs):
 	field(rhs.field),
-	sizex(rhs.sizex),
-	sizey(rhs.sizey),
-	pitchx(rhs.pitchx),
-	pitchy(rhs.pitchy),
 	wvl(rhs.wvl),
 	wvnum(rhs.wvnum),
 	jK(rhs.jK),
 	xaxis(rhs.xaxis),
 	yaxis(rhs.yaxis){}
 
-	void setCircAparture(double D,double centx=0, double centy=0){
-		double r = D/2;
-		for(int ii=0;ii<sizey;ii++){
-			for(int jj=0;jj<sizex;jj++){
+	int sizex() const{return xaxis.size();}
+	int sizey() const{return yaxis.size();}
+	double pitchx() const{return xaxis.Pitch();}
+	double pitchy() const{return yaxis.Pitch();}
+	double wavelength() const{return wvl;}
+	void setWavelength(double wavelength){wvl = wavelength;}
+	std::vector<std::complex<double>> getField() const{return field;}
+
+	void setCircAparture(double diamater,double centx=0, double centy=0){
+		double r = diamater/2;
+		for(int ii=0;ii<sizey();ii++){
+			for(int jj=0;jj<sizex();jj++){
 				if(std::sqrt(yaxis[ii]*yaxis[ii]+xaxis[jj]*xaxis[jj]) <= r){
-					field[ii*sizex+jj] = std::complex<double>(1,0);
+					field[ii*sizex()+jj] = std::complex<double>(1,0);
 				}else{
-					field[ii*sizex+jj] = std::complex<double>(0);
+					field[ii*sizex()+jj] = std::complex<double>(0);
 				}
 			}
 		}
 	}
 
-	void setField(const std::vector<std::complex<double>>& srcfield){
-		std::vector<std::complex<double>>::iterator itr_field = field.begin();
-		std::for_each(srcfield.begin(),srcfield.end(),[&itr_field](const std::complex<double>& fieldelement){
-				*itr_field = fieldelement;
-				++itr_field;
-			});
+	//インスタンスの複素振幅分布から任意の点における光波の複素振幅を求める関数
+	std::complex<double> RSpoint(const double z, const double dst_x, const double dst_y, const double dst_xPitch, const double dst_yPitch){
+		const double ZZ = std::pow(z,2);
+
+		const Eigen::ArrayXXcd srcwf = Eigen::Map<Eigen::ArrayXXcd>(field.data(),sizey(),sizex());
+		const Eigen::ArrayXXd srcax_y = Eigen::Map<Eigen::ArrayXd>(yaxis.data(),yaxis.size()).replicate(1, sizex());
+		const Eigen::ArrayXXd srcax_x = Eigen::Map<Eigen::ArrayXd>(xaxis.data(),xaxis.size()).transpose().replicate(sizey(), 1);
+
+		Eigen::ArrayXXd r01 = Eigen::ArrayXXd::Zero(yaxis.size(),xaxis.size());
+
+		//レイリーゾンマーフェルト積分の計算
+		r01 = (ZZ+(srcax_x-dst_x).square()+(srcax_y-dst_y).square()).sqrt();
+		std::complex<double> U0 = (srcwf*((jK*r01).exp()/r01.square())).sum();
+		U0 *= (z/(imunt*wvl))*(dst_xPitch*dst_yPitch)*(this->xaxis.Pitch()/dst_xPitch)*(this->yaxis.Pitch()/dst_yPitch);
+
+		return U0;
 	}
 
-	const std::vector<std::complex<double>>& getField(){
-		return field;
+	std::complex<double> RSpoint(const double& z, const double& dst_x, const double& dst_y, const double& dst_xPitch, const double& dst_yPitch, const Eigen::ArrayXXcd& srcwf, const Eigen::ArrayXXd& srcax_y, const Eigen::ArrayXXd& srcax_x) const{
+		Eigen::ArrayXXd r01 = Eigen::ArrayXXd::Zero(yaxis.size(),xaxis.size());
+
+		//レイリーゾンマーフェルト積分の計算
+		r01 = (std::pow(z,2)+(srcax_x-dst_x).square()+(srcax_y-dst_y).square()).sqrt();
+		std::complex<double> U0 = (srcwf*((jK*r01).exp()/r01.square())).sum();
+		U0 *= (z/(imunt*wvl))*(dst_xPitch*dst_yPitch)*(this->xaxis.Pitch()/dst_xPitch)*(this->yaxis.Pitch()/dst_yPitch);
+
+		return U0;
 	}
 
-	//伝搬計算では計算後にfieldを上書きする。
+	//伝搬先スクリーンの計算後には計算後にfieldを上書きする。
 	void RSprop(const double z){
 		RSprop(z, xaxis, yaxis);
 	}
@@ -213,12 +177,12 @@ public:
 		std::vector<std::complex<double>> dst_field(distax_x.size()*distax_y.size(),std::complex<double>(0));
 		const double ZZ = std::pow(z,2);
 
-		const Eigen::ArrayXXcd srcwf = Eigen::Map<Eigen::ArrayXXcd>(field.data(),sizey,sizex);
-		const Eigen::ArrayXXd srcax_y = Eigen::Map<Eigen::ArrayXd>(yaxis.data(),yaxis.size()).replicate(1, sizex);
-		const Eigen::ArrayXXd srcax_x = Eigen::Map<Eigen::ArrayXd>(xaxis.data(),xaxis.size()).transpose().replicate(sizey, 1);
+		const Eigen::ArrayXXcd srcwf = Eigen::Map<Eigen::ArrayXXcd>(field.data(),sizey(),sizex());
+		const Eigen::ArrayXXd srcax_y = Eigen::Map<Eigen::ArrayXd>(yaxis.data(),yaxis.size()).replicate(1, sizex());
+		const Eigen::ArrayXXd srcax_x = Eigen::Map<Eigen::ArrayXd>(xaxis.data(),xaxis.size()).transpose().replicate(sizey(), 1);
 
 		Eigen::ArrayXXcd m_dst_field = Eigen::ArrayXXcd::Zero(distax_y.size(),distax_x.size());
-		Eigen::ArrayXXd r01 = Eigen::ArrayXXd::Zero(distax_y.size(),distax_x.size());
+		Eigen::ArrayXXd r01 = Eigen::ArrayXXd::Zero(yaxis.size(),xaxis.size());
 
 		//レイリーゾンマーフェルト積分の計算
 		const int dsize_x = distax_x.size();
@@ -248,19 +212,19 @@ public:
 	}
 
 	void RSprop_nosimd(double z){
-		std::vector<std::complex<double>> distwf(sizey*sizex, std::complex<double>(0));
+		std::vector<std::complex<double>> distwf(sizey()*sizex(), std::complex<double>(0));
 		std::vector<double> distax_x = xaxis;
 		std::vector<double> distax_y = yaxis;
 
 		#ifndef SWPL_UNUSE_OPENMP
 			#pragma omp parallel for
 		#endif
-		for(int ii=0;ii<sizey;ii++){
-			for(int jj=0;jj<sizex;jj++){
-				for(int kk=0;kk<sizey;kk++){
-					for(int ll=0;ll<sizex;ll++){
+		for(int ii=0;ii<sizey();ii++){
+			for(int jj=0;jj<sizex();jj++){
+				for(int kk=0;kk<sizey();kk++){
+					for(int ll=0;ll<sizex();ll++){
 						double r01 = std::sqrt(std::pow(z,2) + std::pow(distax_x[jj]-xaxis[ll],2) + std::pow(distax_y[ii]-yaxis[kk],2));
-						distwf[ii*sizex+jj] += (z/(imunt*wvl))*field[kk*sizex+ll]*(std::exp(imunt*wvnum*r01)/(r01*r01))*(pitchy*pitchx);
+						distwf[ii*sizex()+jj] += (z/(imunt*wvl))*field[kk*sizex()+ll]*(std::exp(imunt*wvnum*r01)/(r01*r01))*(pitchy()*pitchx());
 					}
 				}
 			}
@@ -268,14 +232,14 @@ public:
 		field = distwf;
 	}
 
-	FieldAxis const getXaxis(){return xaxis;}
-	FieldAxis const getYaxis(){return yaxis;}
+	FieldAxis getXaxis() const{return xaxis;}
+	FieldAxis getYaxis() const{return yaxis;}
 
 	void showWFcons(){
 		int ii=0;
 		std::for_each(field.begin(),field.end(),[&ii,this](std::complex<double> num){
 				std::cout<<num<<", "<<std::flush;
-				if((ii+1)%sizex==0){
+				if((ii+1)%sizex()==0){
 					std::cout<<std::endl;
 				}ii++;
 			});
